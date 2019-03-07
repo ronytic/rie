ad1559dda5c41566a87ece3bc370e55057b3e358
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-06 23:41:36 -0400

    Create submenu.php

---
+++ class/submenu.php
@@ -0,0 +1,23 @@
+<?php
+include_once("bd.php");
+class submenu extends bd{
+	var $tabla="submenu";
+	function mostrar($Nivel,$Menu){
+		$this->campos=array('Nombre','Url','Icono');
+		switch($Nivel){
+			case "1":{return $this->getRecords(" SuperAdmin=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+			case "2":{return $this->getRecords(" Administrador=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+			case "3":{return $this->getRecords(" Vendedor=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+			case "4":{return $this->getRecords(" Cliente=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+		}
+	}
+	function verificar($Directorio,$Nivel){
+		switch($Nivel){
+			case "1":{return $this->getRecords("Url='$Directorio' and SuperAdmin=1 and Activo=1","Orden");}break;
+			case "2":{return $this->getRecords("Url='$Directorio' and  Administrador=1 and Activo=1","Orden");}break;
+			case "3":{return $this->getRecords("Url='$Directorio' and  Vendedor=1 and Activo=1","Orden");}break;
+			case "4":{return $this->getRecords("Url='$Directorio' and  Cliente=1 and Activo=1","Orden");}break;
+		}
+	}
+}
+?>
