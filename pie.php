4686992564af1d66c10ec759c6910233a35dcb7e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-06 23:41:44 -0400

    Create pie.php

---
+++ pie.php
@@ -0,0 +1,31 @@
+</div>
+            <div class="footer">
+                <div class="pull-right" title="Cel: (591)-73230568">
+                    Sistema Desarrollado por: <a href="http://fb.com/ronaldnina" target="_blank" title="Cel: (591)-73230568"><strong>Ronald Nina Layme</strong></a>.
+                </div>
+                <div>
+                    <strong>Todos los Derechos Reservados</strong> Sistema de Administración Contable Plus &copy; 2015 - <?php echo date("Y")?>
+                </div>
+            </div>
+
+        </div>
+        </div>
+    <script language="javascript">
+        var folder="<?php echo $folder?>";
+    </script>
+    <!-- Principal scripts -->
+
+    <script src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
+    <script src="<?php echo $folder?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?php echo $folder?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Plugins Modificados Javascript -->
+    <script src="<?php echo $folder?>js/core/inspinia.js"></script>
+    <script src="<?php echo $folder?>js/plugins/form/jquery.form.js"></script>
+    <script src="<?php echo $folder?>js/plugins/busqueda/busqueda.js"></script>
+    <!-- <script src="<?php echo $folder?>js/plugins/pace/pace.min.js"></script>-->
+
+
+</body>
+
+</html>
