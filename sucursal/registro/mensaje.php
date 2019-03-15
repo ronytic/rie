6f41f1fda27b74c756bac6273f2fb20bc11ba8c5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-15 13:18:09 -0400

    Create mensaje.php

---
+++ sucursal/registro/mensaje.php
@@ -0,0 +1,27 @@
+<?php
+include_once("../../login/check.php");
+$CodDiario=isset($_GET['Cod'])?$_GET['Cod']:'';
+$url="reporte.php?Cod=".$CodDiario;
+$titulo="Registro de Sucursal";
+$folder="../../";
+?>
+<?php include_once($folder."cabecerahtml.php");?>
+<?php include_once($folder."cabecera.php");?>
+	<div class="alert alert-<?=$tipomensaje;?>">
+        <ul>
+            <?php
+            if (isset($mensaje)) {
+                foreach($mensaje as $m){
+                    ?>
+                    <li>
+                        <h3><?=$m;?></h3>
+                    </li>
+                    <?php
+                }
+            }
+            ?>
+        </ul>
+        <a href="./" class="btn btn-primary">Registrar Nueva Sucursal</a>
+        <a href="./listar.php" class="btn btn-warning">Listar Sucursales</a>
+    </div>
+<?php include_once($folder."pie.php");?>
