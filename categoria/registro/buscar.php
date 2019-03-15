03f156ee6b5a80109e65fede665b877bb3015649
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-15 19:06:46 -0400

    Listado de Categorias

---
+++ categoria/registro/buscar.php
@@ -0,0 +1,57 @@
+<?php
+include_once("../../login/check.php");
+
+
+$Nombre=$_POST['Nombre'];
+
+include_once("../../class/categoria.php");
+$categoria=new categoria;
+$cat=$categoria->mostrarTodoRegistro("Nombre LIKE '$Nombre%'",1,"",1);
+//print_r($di);
+
+
+?>
+<table class="table table-hover table-bordered table-striped table-table-condensed">
+	<thead>
+    	<tr>
+        	<th width="10">N</th>
+        	<th width="">Nombre</th>
+            <th width="50"></th>
+            <th width="50"></th>
+
+        </tr>
+    </thead>
+	<tbody>
+	<?php
+	if(count($cat)==0){
+
+		?>
+		<tr>
+			<td colspan="5">
+			  No Existen Registros de Categorias
+			</td>
+		</tr>
+	<?php
+	}
+	$i=0;
+	foreach($cat as $d){$i++;
+        ?>
+        	<tr>
+            	<td><?php echo $i;?></td>
+            	<td><?php echo ($d['Nombre'])?></td>
+
+                <td>
+                	<a href="modificar.php?Cod=<?php echo $d['CodCategoria']?>"  class="btn btn-primary btn-xs " title="Modificar" rel="<?php echo $d['CodCategoria']?>">
+                    	<i class="fa fa-pencil"></i>
+                    </a>
+                </td>
+                <td>
+                	<a href="eliminar.php?Cod=<?php echo $d['CodCategoria']?>" class="btn btn-danger btn-xs eliminarDatos" title="Eliminar" rel="<?php echo $d['CodCategoria']?>">
+                    	<i class="fa fa-trash"></i>
+                    </a>
+                </td>
+            </tr>
+        <?php
+    }?>
+</tbody>
+</table>
