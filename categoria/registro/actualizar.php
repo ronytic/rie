ecca4f7813cd36cfa39e4ba8a87fc270a5733936
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-15 19:06:33 -0400

    Modificar Categoria

---
+++ categoria/registro/actualizar.php
@@ -0,0 +1,20 @@
+<?php
+require_once("../../login/check.php");
+if(isset($_POST)){
+    $Cod=$_POST['Cod'];
+    $Nombre=$_POST['Nombre'];
+    $valores=array("Nombre"=>"'$Nombre'",
+                    );
+    include_once("../../class/categoria.php");
+    $categoria=new categoria;
+    $res=$categoria->actualizarRegistro($valores,"CodCategoria=$Cod");
+    if($res){
+        $mensaje[]="La categoria fue modificó correctamente";
+        $tipomensaje="success";
+    }else{
+        $mensaje[]="Error al modificar la categoria";
+        $tipomensaje="danger";
+    }
+    include_once("mensaje.php");
+}
+?>
