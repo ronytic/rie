625ab4fcf1528e7df058e2bf06c8c0545a0a1e32
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-17 18:48:35 -0400

    Update cabecerahtml.php

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -57,7 +57,7 @@ $submenu=new submenu;
 
     <link href="<?php echo $folder?>css/core/animate.css" rel="stylesheet">
     <link href="<?php echo $folder?>css/core/style.css" rel="stylesheet">
-    <link href="<?php echo $folder?>css/core/core.css" rel="stylesheet">
+    <link href="<?php echo $folder?>css/core/core.css?1" rel="stylesheet">
     <link href="<?php echo $folder?>css/core/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet">
 
 
