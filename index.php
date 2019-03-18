8514e0ac03ae09cb3b66b010c5e539ddc9e9e91a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-18 00:37:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ $folder="";
 <?php require_once("cabecera.php");?>
 <div class="panel">
   <div class="panel-body">
-    <div class="">
-      <a href="librodiario/registro/" class="btn btn-info">Lista Actualizada</a>
-      <!-- <a href="librodiario/registro/" class="btn btn-danger">Listado de Diarios</a> -->
+    <div class="text-center">
+      <a href="lista/" class="btn btn-primary btn-lg"> <i class="fa fa-file"></i> Ver Lista Actualizada de Productos </a>
     </div>
     <br>
     <div class="">
+      <!-- <a href="librodiario/registro/" class="btn btn-danger">Listado de Diarios</a> -->
       <!-- <a href="reportes/balanza/" class="btn btn-success">Balanza de Pagos</a> -->
       <!-- <a href="reportes/balanzaparcial/" class="btn btn-success">Balanza Parcial</a> -->
     </div>
