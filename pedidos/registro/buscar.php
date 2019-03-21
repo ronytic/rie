8cf013bccfd36360e5e06ef7e56350211636ad45
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-03-21 12:11:28 -0400

    Listado de Pedidos

---
+++ pedidos/registro/buscar.php
@@ -0,0 +1,120 @@
+<?php
+include_once("../../login/check.php");
+
+// print_r($_POST);
+
+$CodSucursal=$_POST['CodSucursal'];
+$CodCategoria=$_POST['CodCategoria'];
+$Marca=$_POST['Marca'];
+$Nombre=$_POST['Nombre'];
+$NroPedido=$_POST['NroPedido'];
+$FechaPedido=$_POST['FechaPedido'];
+$FechaEntrega=$_POST['FechaEntrega'];
+$Estado=$_POST['Estado'];
+$FechaPedido=$FechaPedido!=""?$FechaPedido:"%";
+$FechaEntrega=$FechaEntrega!=""?$FechaEntrega:"%";
+$NroPedido=$NroPedido !=""?$NroPedido :"%";
+
+include_once("../../class/pedido.php");
+$pedido=new pedido;
+$su=$pedido->mostrarTodoRegistro("Estado='$Estado' and CodSucursal LIKE '$CodSucursal' and CodCategoria LIKE '$CodCategoria' and Marca LIKE '$Marca%' and Nombre LIKE '%$Nombre%' and NroPedido LIKE '$NroPedido' and FechaPedido LIKE '$FechaPedido' and FechaEntrega LIKE '$FechaEntrega'",1,"FechaEntrega,Nombre",1);
+//print_r($di);
+
+
+include_once("../../class/categoria.php");
+$categoria=new categoria;
+include_once("../../class/sucursal.php");
+$sucursal=new sucursal;
+?>
+<table class="table table-hover table-bordered table-striped table-table-condensed">
+	<thead>
+    	<tr>
+        	<th width="10">N</th>
+        	<th width="100">Datos</th>
+        	<th width="100">Detalles</th>
+
+            <th width="40"></th>
+            <th width="40"></th>
+
+        </tr>
+    </thead>
+	<tbody>
+	<?php
+	if(count($su)==0){
+
+		?>
+		<tr>
+			<td colspan="5">
+			  No existen registros de los pedidos buscados
+			</td>
+		</tr>
+	<?php
+	}
+	$i=0;
+	foreach($su as $d){$i++;
+		$c=$categoria->mostrarTodoRegistro("CodCategoria=".$d['CodCategoria']);
+		$c=array_shift($c);
+		$s=$sucursal->mostrarTodoRegistro("CodSucursal=".$d['CodSucursal']);
+		$s=array_shift($s);
+        ?>
+        	<tr>
+            	<td><?php echo $i;?></td>
+            	<td class="small">
+					<table class="table table-bordered">
+
+						<tr>
+							<td class="resaltar" colspan="2"><?php echo ($d['Nombre'])?></td>
+						</tr>
+						<tr>
+							<td class=" resaltar" width="30%">Categoria: </td>
+							<td class=""><?php echo ($c['Nombre'])?></td>
+						</tr>
+						<tr>
+							<td class=" resaltar">Marca: </td>
+							<td class=""><?php echo ($d['Marca'])?></td>
+						</tr>
+						<tr>
+							<td class=" resaltar">Sucursal: </td>
+							<td class=""><?php echo ($s['Nombre'])?></td>
+						</tr>
+					</table>
+
+
+				</td>
+
+				<td class="small">
+					<table class="table table-bordered">
+
+						<tr>
+							<td class="der resaltar" width="50%">Detalle: </td>
+							<td class=""><?php echo ($d['Detalle'])?></td>
+						</tr>
+						<tr>
+							<td class="der resaltar">Nro Pedido: </td>
+							<td class=""><?php echo ($d['NroPedido'])?></td>
+						</tr>
+						<tr>
+							<td class="der resaltar">Fecha de Pedido: </td>
+							<td class=""><?php echo fecha2str($d['FechaPedido'])?></td>
+						</tr>
+						<tr>
+							<td class="der resaltar">Fecha de Entrega: </td>
+							<td class=""><?php echo fecha2str($d['FechaEntrega'])?></td>
+						</tr>
+					</table>
+				</td>
+                <td class="text-center">
+                	<a href="modificar.php?Cod=<?php echo $d['CodPedido']?>"  class="btn btn-primary btn-xs " title="Modificar" rel="<?php echo $d['CodPedido']?>">
+                    	<i class="fa fa-pencil"></i>
+                    </a>
+                </td>
+                <td class="text-center">
+                	<a href="eliminar.php?Cod=<?php echo $d['CodPedido']?>" class="btn btn-danger btn-xs eliminarDatos" title="Eliminar" rel="<?php echo $d['CodPedido']?>">
+                    	<i class="fa fa-trash"></i>
+                    </a>
+                </td>
+            </tr>
+        <?php
+    }?>
+</tbody>
+</table>
---
+++ pedidos/registro/listar.php
@@ -0,0 +1,125 @@
+<?php
+include_once("../../login/check.php");
+
+include_once("../../class/sucursal.php");
+$sucursal=new sucursal;
+$suc=$sucursal->mostrarTodoRegistro("",1,"Nombre");
+$suc=todoLista($suc,"CodSucursal","Nombre");
+$suc=array_unshift_assoc($suc,"%","Todos");
+
+include_once("../../class/categoria.php");
+$categoria=new categoria;
+$cate=$categoria->mostrarTodoRegistro("",1,"Nombre");
+$cat=todoLista($cate,"CodCategoria","Nombre");
+$cat=array_unshift_assoc($cat,"%","Todos");
+// echo "<pre>";
+// print_r($cat);
+// echo "</pre>";
+include_once("../../class/marca.php");
+$marca=new marca;
+$marc=$marca->mostrarTodoRegistro("",1,"Nombre");
+$mar=todoLista($marc,"CodMarca","Nombre");
+$mar=array_unshift_assoc($mar,"%","Todos");
+
+$est=array("0"=>"No Entregado","1"=>"Entregado");
+$titulo="Listado de Pedidos";
+$folder="../../";
+
+
+
+?>
+<?php include_once($folder."cabecerahtml.php");?>
+<script>
+$(document).ready(function(){
+	$(document).ajaxStart(function() {
+		$("#respuesta").html('Cargando...');
+	});
+	$("#formulario").submit(function(e) {
+        e.preventDefault();
+
+		var CodSucursal=$("[name=CodSucursal]").val();
+		var CodCategoria=$("[name=CodCategoria]").val();
+		var Marca=$("[name=Marca]").val();
+		var Nombre=$("[name=Nombre]").val();
+		var NroPedido=$("[name=NroPedido]").val();
+		var FechaPedido=$("[name=FechaPedido]").val();
+		var FechaEntrega=$("[name=FechaEntrega]").val();
+		var Estado=$("[name=Estado]").val();
+		$.post("buscar.php",{Estado:Estado,'CodSucursal':CodSucursal,CodCategoria:CodCategoria,Marca:Marca,Nombre:Nombre,NroPedido:NroPedido,FechaPedido:FechaPedido,FechaEntrega:FechaEntrega},function(data){
+			$("#respuesta").html(data);
+		});
+    });
+	$(document).on("click",".eliminarDatos",function(e){
+		e.preventDefault();
+		swal("¿Esta Seguro de Eliminar este Registro?",{
+  			buttons: {
+				cancel: true,
+				confirm: {
+					text:"Aceptar",
+					value:'ok'
+				}
+  			}
+		}).then((value)=>{
+			switch (value) {
+				case "ok":{
+					var Cod=$(this).attr("rel");
+					$.post("eliminar.php",{"Cod":Cod},function(data){
+						$("#formulario").submit();
+					});
+				}break;
+			}
+		});
+	});
+
+});
+</script>
+<?php include_once($folder."cabecera.php");?>
+<div class="panel">
+	<div class="panel-body">
+		<form action="buscar.php" method="post" id="formulario">
+
+        	<div class="table-responsive ">
+				<table class="table">
+					<tr>
+						<td>
+							<?php
+                        if(!in_array( $_SESSION['NivelAcceso'],array(1,2))){
+							$val="disabled";
+                            echo campo("CodSucursal","hidden",$_SESSION['CodSucursal']);
+                        }else{
+							$val="";
+                        }
+						?>
+						Sucursal
+                        <?=campo("CodSucursal","select",$suc,"form-control",1,"",0,array($val=>$val),$_SESSION['CodSucursal'],1);?>
+						</td>
+						<td>Estado<?=campo("Estado","select",$est,"form-control",1,"",1,array(),0,0);?></td>
+					</tr>
+
+					<tr>
+						<td>Categoria<?=campo("CodCategoria","select",$cat,"form-control",1,"",1,array(),0,0);?></td>
+						<td>Marca<?=campo("Marca","text","","form-control",0,"",1,array(),0,0);?></td>
+						<td>Nombre<input type="text" name="Nombre" class="form-control"></td>
+					</tr>
+					<tr>
+						<td>Nro Pedido<input type="text" name="NroPedido" class="form-control"></td>
+						<td>Fecha de Pedido<input type="date" name="FechaPedido" class="form-control"></td>
+						<td>Fecha de Entrega<input type="date" name="FechaEntrega" class="form-control"></td>
+
+					</tr>
+					<tr>
+					<td><input type="submit" value="Buscar" class="btn btn-primary"></td>
+					</tr>
+				</table>
+			</div>
+
+
+        </form>
+
+	</div>
+</div>
+<div class="panel">
+	<div class="panel-body table-responsive" id="respuesta">
+	</div>
+</div>
+<?php include_once($folder."pie.php");?>
