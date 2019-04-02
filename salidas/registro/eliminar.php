cba3b08f67da4c3efb3c5682200af056835f4c92
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-02 19:06:08 -0400

    Eliminar Salida

--- salidas/registro/eliminar.php
+++ salidas/registro/eliminar.php
@@ -5,6 +5,12 @@ $Estado=$_POST['Estado'];
 include_once("../../class/salida.php");
 $salida=new salida;
 $salida->actualizarRegistro(array("Estado"=>"'$Estado'"),"CodSalida=".$Cod);
+
+include_once("../../class/ingreso.php");
+$ingreso=new ingreso;
+$ingreso->actualizarRegistro(array("Estado"=>"'$Estado'"),"CodSalida=".$Cod);
+
+
 // $producto->eliminarRegistro("Codproducto=".$Cod);
 
 ?>
