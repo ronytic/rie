5d5348937894fb06f2e524e115e4845f10a97348
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-04 16:55:40 -0400

    Listado de usuarios

---
+++ usuario/registro/buscar.php
@@ -0,0 +1,121 @@
+<?php
+include_once("../../login/check.php");
+
+
+$CodSucursal=$_POST['CodSucursal'];
+$NivelAcceso=$_POST['NivelAcceso'];
+$Nombres=$_POST['Nombres'];
+$Apellidos=$_POST['Apellidos'];
+$Ci=$_POST['Ci'];
+
+include_once("../../class/usuario.php");
+$usuario=new usuario;
+$su=$usuario->mostrarTodoRegistro("NivelAcceso!=1 and CodSucursal LIKE '$CodSucursal' and NivelAcceso LIKE '$NivelAcceso' and Nombres LIKE '%$Nombres%' and Apellidos LIKE '%$Apellidos%' and Ci LIKE '%$Ci%'",1,"Apellidos,Nombres",1);
+//print_r($di);
+
+
+include_once("../../class/sucursal.php");
+$sucursal=new sucursal;
+include_once("../../class/acceso.php");
+$acceso=new acceso;
+?>
+<table class="table table-hover table-bordered table-striped table-table-condensed">
+	<thead>
+    	<tr>
+        	<th width="10">N</th>
+        	<th width="100">Datos</th>
+
+
+        	<th width="100">Detalles</th>
+			<?php
+					if(in_array( $_SESSION['NivelAcceso'],array(1,2))){
+				?>
+            <th width="40"></th>
+            <th width="40"></th>
+			<?php
+			}
+			?>
+        </tr>
+    </thead>
+	<tbody>
+	<?php
+	if(count($su)==0){
+
+		?>
+		<tr>
+			<td colspan="5">
+			  No existen registros de los usuarios buscados
+			</td>
+		</tr>
+	<?php
+	}
+	$i=0;
+	foreach($su as $d){$i++;
+		$s=$sucursal->mostrarTodoRegistro("CodSucursal=".$d['CodSucursal']);
+		$s=array_shift($s);
+		$a=$acceso->mostrarTodoRegistro("CodAcceso=".$d['NivelAcceso']);
+		$a=array_shift($a);
+        ?>
+        	<tr>
+            	<td><?php echo $i;?></td>
+            	<td class="small">
+					<table class="table table-bordered">
+						<tr>
+							<td class=" resaltar" width="30%">Apellidos: </td>
+							<td class=""><?php echo ($d['Apellidos'])?></td>
+						</tr>
+						<tr>
+							<td class=" resaltar">Nombres: </td>
+							<td class=""><?php echo ($d['Nombres'])?></td>
+						</tr>
+						<tr>
+							<td class=" resaltar">Carnet: </td>
+							<td class=""><?php echo ($d['Ci'])?></td>
+						</tr>
+
+
+					</table>
+
+
+				</td>
+
+				<td class="small">
+					<table class="table table-bordered">
+
+						<tr>
+							<td class="der resaltar" width="30%">Sucursal: </td>
+							<td class=""><span class="badge badge-danger"><?php echo ($s['Nombre']??"Todos")?></span></td>
+						</tr>
+						<tr>
+							<td class="der resaltar">Nivel Acceso: </td>
+							<td class=""><span class="badge badge-primary"><?php echo ($a['Nombre'])?></span></td>
+						</tr>
+						<tr>
+							<td class="der resaltar">Usuario: </td>
+							<td class=""><?php echo ($d['Usuario'])?></td>
+						</tr>
+
+					</table>
+				</td>
+
+				<?php
+					if(in_array( $_SESSION['NivelAcceso'],array(1,2))){
+				?>
+                <td class="text-center">
+                	<a href="modificar.php?Cod=<?php echo $d['Cod']?>"  class="btn btn-primary btn-xs " title="Modificar" rel="<?php echo $d['Cod']?>">
+                    	<i class="fa fa-pencil"></i>
+                    </a>
+                </td>
+                <td class="text-center">
+                	<a href="eliminar.php?Cod=<?php echo $d['Cod']?>" class="btn btn-danger btn-xs eliminarDatos" title="Eliminar" rel="<?php echo $d['Cod']?>">
+                    	<i class="fa fa-trash"></i>
+                    </a>
+                </td>
+				<?php
+					}
+				?>
+            </tr>
+        <?php
+    }?>
+</tbody>
+</table>
---
+++ usuario/registro/listar.php
@@ -0,0 +1,99 @@
+<?php
+include_once("../../login/check.php");
+
+
+include_once("../../class/acceso.php");
+$acceso=new acceso;
+$acc=$acceso->mostrarTodoRegistro("Visible=1",1,"Nombre");
+$acc=todoLista($acc,"CodAcceso","Nombre");
+$acc=array_unshift_assoc($acc,"%","Todos");
+// echo "<pre>";
+// print_r($cat);
+// echo "</pre>";
+include_once("../../class/sucursal.php");
+$sucursal=new sucursal;
+$suc=$sucursal->mostrarTodoRegistro("",1,"Nombre");
+$suc=todoLista($suc,"CodSucursal","Nombre");
+$suc=array_unshift_assoc($suc,"%","Todos");
+
+
+$titulo="Listado de Usuarios";
+$folder="../../";
+
+
+
+?>
+<?php include_once($folder."cabecerahtml.php");?>
+<script>
+$(document).ready(function(){
+	$(document).ajaxStart(function() {
+		$("#respuesta").html('Cargando...');
+	});
+	$("#formulario").submit(function(e) {
+        e.preventDefault();
+
+		var CodSucursal=$("[name=CodSucursal]").val();
+		var NivelAcceso=$("[name=NivelAcceso]").val();
+		var Nombres=$("[name=Nombres]").val();
+		var Apellidos=$("[name=Apellidos]").val();
+		var Ci=$("[name=Ci]").val();
+		$.post("buscar.php",{'CodSucursal':CodSucursal,NivelAcceso:NivelAcceso,Nombres:Nombres,Apellidos:Apellidos,Ci:Ci},function(data){
+			$("#respuesta").html(data);
+		});
+	})
+	.submit();
+	$(document).on("click",".eliminarDatos",function(e){
+		e.preventDefault();
+		swal("¿Esta Seguro de Eliminar este Registro?",{
+  			buttons: {
+				cancel: true,
+				confirm: {
+					text:"Aceptar",
+					value:'ok'
+				}
+  			}
+		}).then((value)=>{
+			switch (value) {
+				case "ok":{
+					var Cod=$(this).attr("rel");
+					$.post("eliminar.php",{"Cod":Cod},function(data){
+						$("#formulario").submit();
+					});
+				}break;
+			}
+		});
+	});
+
+});
+</script>
+<?php include_once($folder."cabecera.php");?>
+<div class="panel">
+	<div class="panel-body">
+		<form action="buscar.php" method="post" id="formulario">
+
+        	<div class="table-responsive">
+				<table class="table">
+				            	<tr>
+									<td>Sucursal<?=campo("CodSucursal","select",$suc,"form-control",1,"",1,array(),0,0);?></td>
+									<td>Nivel de Acceso<?=campo("NivelAcceso","select",$acc,"form-control",0,"",1,array(),0,0);?></td>
+				                    <td>Nombres<input type="text" name="Nombres" class="form-control"></td>
+				                    <td>Apellidos<input type="text" name="Apellidos" class="form-control"></td>
+				                    <td>Carnet<input type="text" name="Ci" class="form-control"></td>
+
+								</tr>
+								<tr>
+								<td><input type="submit" value="Buscar" class="btn btn-primary"></td>
+								</tr>
+				            </table>
+			</div>
+
+
+        </form>
+
+	</div>
+</div>
+<div class="panel">
+	<div class="panel-body table-responsive" id="respuesta">
+	</div>
+</div>
+<?php include_once($folder."pie.php");?>
