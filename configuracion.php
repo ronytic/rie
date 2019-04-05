ccd086e00032fed7104706e3f4b819b4f32de626
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-05 00:48:17 -0400

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -1,4 +1,19 @@
 <?php
 $directory="rie/";		//Sub Directorio, Ej: "sistemacontable/"
 
+
+$CodigoNotificación='
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+var OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+OneSignal.init({
+appId: "111a4032-de45-4f48-994e-801346a9c6dc",
+});
+});
+</script>
+';
+$app_id="111a4032-de45-4f48-994e-801346a9c6dc";
+$app_key="YWE1NjdmZTMtNjk0NC00OTg3LWI4NjgtOGYxMTRhZDY0NWZm";
+
 ?>
