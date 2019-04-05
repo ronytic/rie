f6704c400abf5eaf22d2b75af1e0c5b9534e6225
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-04 21:36:46 -0400

    Update check.php

--- login/check.php
+++ login/check.php
@@ -8,7 +8,13 @@ if(!(isset($_SESSION["LoginSistemaRie"]) && $_SESSION['LoginSistemaRie']==1)){
 	include_once(RAIZ."funciones/url.php");
 	header("Location:".url_base().$directory."login/?u=".$_SERVER['PHP_SELF']);
 }else{
-
+	include_once(RAIZ."funciones/url.php");
 	include_once(RAIZ."funciones/funciones.php");
+	include_once(RAIZ."class/menu.php");
+	$menu=new menu();
+	$s_url= separar_url($directory,url_sub());
+	$res_menu=$menu->obtenerOpcion($s_url[0]);
+	setcookie("Opcion","asd",time()-1);
+    setcookie("Opcion",$res_menu['CodMenu']);
 }
 ?>
