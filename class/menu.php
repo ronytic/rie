35016eb1854d23a3e8beb29daf30d5f6c15c683e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-04 20:17:27 -0400

    Update menu.php

--- class/menu.php
+++ class/menu.php
@@ -12,6 +12,16 @@ class menu extends bd{
 			case "4":{return $this->getRecords(" Cliente=1 and Activo=1 $Posicion","Orden");}break;
 		}
 	}
+	function inicio($Nivel,$Posicion=""){
+		$this->campos=array('CodMenu','Nombre','Url','Icono');
+		$Posicion=(!empty($Posicion))?" and Posicion='$Posicion'":"";
+		switch($Nivel){
+			case "1":{return $this->getRecords("Inicio!=0 and SuperAdmin=1 and Activo=1 $Posicion","Inicio");}break;
+			case "2":{return $this->getRecords("Inicio!=0 and Administrador=1 and Activo=1 $Posicion","Inicio");}break;
+			case "3":{return $this->getRecords("Inicio!=0 and Vendedor=1 and Activo=1 $Posicion","Inicio");}break;
+			case "4":{return $this->getRecords("Inicio!=0 and Cliente=1 and Activo=1 $Posicion","Inicio");}break;
+		}
+	}
 	function verificar($Directorio,$Nivel){
 		switch($Nivel){
 			case "1":{return $this->getRecords("Url='$Directorio' and SuperAdmin=1 and Activo=1","Orden");}break;
