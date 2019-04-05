e414ea4b158f79ac3c7c7adfa54c124a78cfa9e4
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-04 20:17:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,37 @@ $folder="";
       <a href="lista/" class="btn btn-primary btn"> <i class="fa fa-file"></i> Ver Lista Actualizada de Productos </a>
     </div>
     <br>
-    <div class="">
-      <!-- <a href="librodiario/registro/" class="btn btn-danger">Listado de Diarios</a> -->
+    <div class="row">
+      <?php
+        foreach($menu->inicio($_SESSION['NivelAcceso']) as $m_i){
+      ?>
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+        <fieldset>
+          <legend><?=$m_i['Nombre'];?></legend>
+            <?php
+              $sm_o=0;
+
+
+              foreach($submenu->mostrar($_SESSION['NivelAcceso'],$m_i['CodMenu']) as $subm_i){
+                $sm_o++;
+                switch($sm_o){
+                  case 1:{$sm_t="success";}break;
+                  case 2:{$sm_t="primary";}break;
+                  case 3:{$sm_t="info";$sm_o=0;}break;
+                }
+            ?>
+            <a href="<?=$m_i['Url'];?><?=$subm_i['Url'];?>" class="btn btn-<?=$sm_t;?>"><?=$subm_i['Nombre'];?></a>
+            <br><br>
+          <?php
+            }
+          ?>
+         </fieldset>
+      </div>
+        <?php
+          }
+        ?>
+
+
       <!-- <a href="reportes/balanza/" class="btn btn-success">Balanza de Pagos</a> -->
       <!-- <a href="reportes/balanzaparcial/" class="btn btn-success">Balanza Parcial</a> -->
     </div>
