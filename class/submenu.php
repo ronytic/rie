55b0999a2b957601d58edd59fa4c5f0dbf85ee89
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-04 20:17:29 -0400

    Update submenu.php

--- class/submenu.php
+++ class/submenu.php
@@ -11,6 +11,15 @@ class submenu extends bd{
 			case "4":{return $this->getRecords(" Cliente=1 and CodMenu=$Menu and Activo=1","Orden");}break;
 		}
 	}
+	function inicio($Nivel,$Menu){
+		$this->campos=array('Nombre','Url','Icono');
+		switch($Nivel){
+			case "1":{return $this->getRecords("Inicio!=0 and SuperAdmin=1 and CodMenu=$Menu and Activo=1","Inicio");}break;
+			case "2":{return $this->getRecords("Inicio!=0 and Administrador=1 and CodMenu=$Menu and Activo=1","Inicio");}break;
+			case "3":{return $this->getRecords("Inicio!=0 and Vendedor=1 and CodMenu=$Menu and Activo=1","Inicio");}break;
+			case "4":{return $this->getRecords("Inicio!=0 and Cliente=1 and CodMenu=$Menu and Activo=1","Inicio");}break;
+		}
+	}
 	function verificar($Directorio,$Nivel){
 		switch($Nivel){
 			case "1":{return $this->getRecords("Url='$Directorio' and SuperAdmin=1 and Activo=1","Orden");}break;
